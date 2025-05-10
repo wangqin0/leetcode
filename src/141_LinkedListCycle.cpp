e46a47692a138b74b05fef9ef25b14cbff773885
Wang Qin <wangqin0@outlook.com>
2025-05-10 12:07:22 -0700

    Add C++ module support

--- src/141_LinkedListCycle.cpp
+++ src/141_LinkedListCycle.cpp
@@ -2,11 +2,7 @@
 #include <vector>
 #include <string>
 
-struct ListNode {
-    int val = 0;
-    ListNode *next = nullptr;
-    ListNode(int val = 0, ListNode* next = nullptr) : val(val), next(next) {}
-};
+import ListNode;
 
 class Solution {
 public:
---
+++ src/Util/ListNode.cppm
@@ -0,0 +1,7 @@
+export module ListNode;
+
+export struct ListNode {
+    int val = 0;
+    ListNode *next = nullptr;
+    ListNode(int val = 0, ListNode* next = nullptr) : val(val), next(next) {}
+};
